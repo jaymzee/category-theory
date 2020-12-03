2bffe9e8f38861ca4fcb9df19bb667dc24d1bb82
jaymzee <jaymzee@users.noreply.github.com>
2020-12-02 19:14:53 -0500

    swing examples

---
+++ examples/swing/LineDrawingExample.java
@@ -0,0 +1,42 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Line2D;
+ 
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+ 
+/**
+ * This program demonstrates how to draw lines using Graphics2D object.
+ * @author www.codejava.net
+ *
+ */
+public class LineDrawingExample extends JFrame {
+    public LineDrawingExample() {
+        super("Lines Drawing Demo");
+        setSize(500, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+
+    void drawLines(Graphics g) {
+        var g2d = (Graphics2D)g;
+        g2d.drawLine(120, 50, 360, 50);
+        g2d.draw(new Line2D.Double(59.2d, 99.8d, 419.1d, 99.8d));
+        g2d.draw(new Line2D.Float(21.50f, 132.50f, 459.50f, 132.50f));
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        drawLines(g);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new LineDrawingExample().setVisible(true);
+            }
+        });
+    }
+}
